ddc35d6ffa8c27a62c1db9d09388c22c4860a5bd
Timothy C Eichler <timeyyy_da_man@hotmail.com>
2024-12-12 21:18:42 +1000

    update next config

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true, // Required for static export
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/zenchor-website' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/zenchor-website/' : '',
 }
 
 
