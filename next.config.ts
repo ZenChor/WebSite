88b2328c5e734b71e238e8e8215098d3934018a9
Timothy C Eichler <timeyyy_da_man@hotmail.com>
2024-12-27 14:42:11 +1000

    multiple builds

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,30 @@
 import type { NextConfig } from "next";
 
+type Environment = 'local' | 'github' | 'custom-domain';
+
+// Get environment from an env variable - default to local
+const DEPLOY_ENV = (process.env.DEPLOY_ENV || 'local') as Environment;
+
+const getBasePath = (env: Environment) => {
+  switch (env) {
+    case 'local':
+      return '';
+    case 'github':
+      return '/WebSite';
+    case 'custom-domain':
+      return '';
+    default:
+      return '';
+  }
+};
+
 const nextConfig = {
   output: 'export',  // Enable static exports
   images: {
     unoptimized: true, // Required for static export
   },
+  basePath: getBasePath(DEPLOY_ENV),
+  assetPrefix: getBasePath(DEPLOY_ENV),
 }
 
 
